19f5fa3089262e6655a3f76acc74c7101b43cda4
Bluezzi <xbluezzi@gmail.com>
2022-09-20 21:35:39 -0500

    just started

---
+++ readme2.md
@@ -0,0 +1,5 @@
+Iphone calendar clone
+//Shows date, time, and lets you set events at a certain date and time
+
+//Additional comments
+-Intro to react
--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { Calendar, dateFnsLocalizer, datefnsLocalizer } from "react-big-calendar";
+import format from "date-fns/format";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import getDay from "date-fns/getDay";
+import "react-big-calendar/lib/css/react-bigcalendar.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+
+const locales{ 
+"en-US": require("date-fns/locale/en-US")
+}
+const localizer = dateFnsLocalizer({
+format, 
+parse, 
+startOfWeek,
+getDay,
+locales
+})
+
+
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2022,9,13)
+    end: new Date(2022,9,13)
+  }
+  {
+    title: "Test",
+    allDay: true,
+    start: new Date(2022,9,13)
+    end: new Date(2022,9,13)
+  }
+  {
+    title: "Date",
+    allDay: true,
+    start: new Date(2022,9,13)
+    end: new Date(2022,9,13)
+  }
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Calendar localizer = {localizer} events = {events} 
+      startAccessor = "start" endAccessor = "end" style = {{height: 500, margin: "50px"}} />
     </div>
   );
 }
